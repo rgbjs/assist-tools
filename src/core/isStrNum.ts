1373bedaa4c85c5c918e8a2b2d178450790633d4
feiYu <946686638@qq.com>
2024-05-03 18:58:39 +0800

    [update] 修复空白字符串bug

--- src/core/isStrNum.ts
+++ src/core/isStrNum.ts
@@ -7,40 +7,42 @@ import isNum from './isNum'
  * - '' 和 NaN 和 Infinity 和 -Infinity 被视为无效数字
  * @param str 任意类型
  * @param strict 更加严格的判断 默认为 true [可选]
- * - 字符串不允许首尾存在空白字符(空格, 制表, 换行 等) 
+ * - 字符串不允许首尾存在空白字符(空格, 制表, 换行 等)
  * - 不允许省略的形式的数字字符串, 例如: '.1' 和 '1.' 不被允许, 数字类型忽略该条规则, 因为数字在使用时会自动补全 0
- * - 字符串不允许以无效的多个 0 开头, 例如: 000001 , 数字类型无视该条规则, 因为数字在使用时会自动舍弃无效的 0 
+ * - 字符串不允许以无效的多个 0 开头, 例如: 000001 , 数字类型无视该条规则, 因为数字在使用时会自动舍弃无效的 0
  */
-const isStrNum = (str: any, strict: boolean = true): boolean => {
-    const type = typeof str
-    if (!(type === 'number' || type === 'string')) return false
-    if (type === 'number') {
-        if (isNum(str)) {
-            return true
-        } else {
-            return false
-        }
-    }
- 
-    if (str === '') return false
-    if (strict) {
-        const reg1 = /\s/g
-        if (reg1.test(str)) return false
-        if (str.includes('.')) {
-            const start = str.substr(0, 2)
-            if (start === '00') return false
-            const reg2 = /[0-9]\.[0-9]/
-            if (!reg2.test(str)) return false
-        } else {
-            if (str.length > 1) {
-                const start = str[0]
-                if (start === '0') return false
-            }
-        }
-    }
+const isStrNum = (str: unknown, strict: boolean = true): boolean => {
+	const type = typeof str
+	if (!(type === 'number' || type === 'string')) return false
 
-    if (isNum(Number(str))) return true
-    return false
-} 
+	// 为数字
+	if (type === 'number') {
+		return isNum(str)
+	}
+
+	if ((str as string).trim() === '') return false
+
+	// 为字符串, 且开启严格模式
+	if (strict) {
+		const reg1 = /\s/g
+		if (reg1.test(str as string)) return false
+
+		if ((str as string).includes('.')) {
+			const start = (str as string).substring(0, 2)
+			if (start === '00') return false
+
+			const reg2 = /[0-9]\.[0-9]/
+			if (!reg2.test(str as string)) return false
+		} else {
+			if ((str as string).length > 1) {
+				const start = (str as string)[0]
+				if (start === '0') return false
+			}
+		}
+	}
+
+	// 普通模式
+	return isNum(Number(str))
+}
 
 export default isStrNum
--- tests/isStrNum.test.ts
+++ tests/isStrNum.test.ts
@@ -7,13 +7,20 @@ describe('isStrNum', () => {
 		expect(isStrNum(1.1)).toBe(true)
 	})
 
+	test('bigint', () => {
+		expect(isStrNum(BigInt(1))).toBe(false)
+	})
+
 	test('NaN', () => {
 		expect(isStrNum(NaN)).toBe(false)
+		expect(isStrNum('NaN', false)).toBe(false)
 	})
 
 	test('Infinity 与 -Infinity', () => {
 		expect(isStrNum(Infinity)).toBe(false)
+		expect(isStrNum('Infinity', false)).toBe(false)
 		expect(isStrNum(-Infinity)).toBe(false)
+		expect(isStrNum('-Infinity', false)).toBe(false)
 	})
 
 	test('普通数字字符串', () => {
@@ -30,6 +37,9 @@ describe('isStrNum', () => {
 		expect(isStrNum('   1.   ', false)).toBe(true)
 		expect(isStrNum('   .1   ', false)).toBe(true)
 		expect(isStrNum('   .0   ', false)).toBe(true)
+		expect(isStrNum('0b10', false)).toBe(true)
+		expect(isStrNum('', false)).toBe(false)
+		expect(isStrNum('   ', false)).toBe(false)
 	})
 
 	test('严格数字字符串', () => {
@@ -41,5 +51,13 @@ describe('isStrNum', () => {
 		expect(isStrNum('1.')).toBe(false)
 		expect(isStrNum('')).toBe(false)
 		expect(isStrNum('1n')).toBe(false)
+		expect(isStrNum('a')).toBe(false)
+		expect(isStrNum('00')).toBe(false)
+		expect(isStrNum('.0')).toBe(false)
+		expect(isStrNum('0.')).toBe(false)
+		expect(isStrNum('-0.')).toBe(false)
+		expect(isStrNum('-')).toBe(false)
+		expect(isStrNum('+')).toBe(false)
+		expect(isStrNum('0b10')).toBe(false)
 	})
 })
