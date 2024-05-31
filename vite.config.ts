b66a26b60f9aefb86d83cf6a6c8c57880ce9dcf1
feiYu <feiyucode@gmail.com>
2024-05-31 16:43:03 +0800

    [update] 优化工程结构

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
 	build: {
 		lib: {
 			entry: './src/main.ts',
 			name: 'assistTools',
-			formats: ['es'],
-			fileName: 'main.es'
+			formats: ['es', 'cjs'],
+			fileName(format, entryName) {
+				return `${entryName}.${format}.js`
+			}
 		}
-	}
+	},
+
+	plugins: [
+		dts({
+			insertTypesEntry: true,
+			afterBuild(emittedFiles) {
+				const rootPath = path.resolve()
+				const reg = /\\/g
+				const p = path.join(rootPath, '/dist/main.es.d.ts').replace(reg, '/')
+				const content = emittedFiles.get(p) as string
+				fs.writeFileSync(path.join(rootPath, '/dist/main.cjs.d.ts'), content)
+			}
+		})
+	]
 })
