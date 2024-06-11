f5065001d9adf8f26a7dcc0003a78d4861c0523b
feiYu <946686638@qq.com>
2024-06-11 23:48:48 +0800

    [update] 移除 dist

--- dist/core/isEffectiveValue.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 判断传入的所有数据是否都为有效值, (null, '', undefined, NaN, Infinity, -Infinity 被视为无效值)
- * @param val 测试的数据
- * @returns
- */
-declare const isEffectiveValue: (...val: any) => boolean;
-export default isEffectiveValue;
--- dist/core/isString.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 判断数据是否为字符串类型
- * @param data 需要判断的数据, 所有传入的参数都通过测试将返回 true, 否则返回 false
- * @returns
- */
-declare const isString: (...data: any) => boolean;
-export default isString;
